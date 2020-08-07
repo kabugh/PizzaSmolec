d0a0eda83344e7548c3cc2e15084d0dced6dbced
kabugh <kabugh00@gmail.com>
2020-08-07 19:03:42 +0200

    PhotoGallery component improved

--- src/components/PhotoGallery.vue
+++ src/components/PhotoGallery.vue
@@ -19,7 +19,8 @@
           "
           data-aos="fade-up"
           data-aos-easing="ease-in-out-quad"
-          :data-aos-delay="200 + i * 100"
+          data-aos-duration="800"
+          :data-aos-delay="animationDelay + i * 100"
         >
           <img
             :src="item.frontImage.fields.file.url"
@@ -34,7 +35,6 @@
             v-else-if="item.url"
           />
           <img :src="item.url" :alt="item" class="image" v-else />
-
           <div
             class="image__description_layer"
             :class="{ light: displayImageOnly && !displayOverlay }"
@@ -73,6 +73,12 @@ export default class PhotoGallery extends Vue {
   @Prop({ default: false, required: true }) zoomedPhotos!: boolean;
   @Prop({ default: false, required: true }) displayOverlay!: boolean;
   @Prop({ default: false }) nestedGallery!: boolean;
+  @Prop({
+    default: 200,
+    required: false,
+    validator: value => value >= 0 && value <= 1000
+  })
+  animationDelay!: number;
 
   index = null;
 
@@ -96,7 +102,7 @@ export default class PhotoGallery extends Vue {
     overflow: hidden;
     .grid {
       display: grid;
-      grid-template-columns: repeat(auto-fill, minmax(12rem, 1fr));
+      grid-template-columns: repeat(auto-fill, minmax(14rem, 1fr));
       grid-auto-rows: 1fr;
       grid-gap: 3vh;
       // &::before {
@@ -126,10 +132,11 @@ export default class PhotoGallery extends Vue {
         width: 100%;
         height: 100%;
         object-fit: cover;
+        background-color: #fff;
       }
       .image__description_layer {
         position: absolute;
-        top: 0;
+        // top: 0;
         bottom: 0;
         left: 0;
         right: 0;
@@ -139,10 +146,10 @@ export default class PhotoGallery extends Vue {
         // opacity: 0;
         @include flex;
         flex-direction: row;
-
-        align-items: flex-end;
         transition: opacity 0.8s cubic-bezier(0.65, 0, 0.35, 1),
           visibility 0.8s cubic-bezier(0.65, 0, 0.35, 1);
+        top: initial;
+        height: 20%;
         &.light {
           background: transparent;
         }
@@ -151,12 +158,12 @@ export default class PhotoGallery extends Vue {
       .description__container {
         transition: transform 0.6s cubic-bezier(0.65, 0, 0.35, 1);
         max-height: 100%;
-        padding: 4vmin;
+        @include flex;
+        // padding: 4vmin;
         text-align: center;
         h1 {
-          font-size: 1.25rem;
+          font-size: 1rem;
           text-transform: capitalize;
-          margin-bottom: 2vh;
         }
         p {
           font-size: 0.875rem;
@@ -165,8 +172,13 @@ export default class PhotoGallery extends Vue {
       }
     }
     @media (min-width: 360px) {
-      .image__container .description__container p {
-        font-size: 1rem;
+      .image__container .description__container {
+        h1 {
+          font-size: 1.25rem;
+        }
+        p {
+          font-size: 1rem;
+        }
       }
     }
     @media (min-width: 768px) {
@@ -179,6 +191,9 @@ export default class PhotoGallery extends Vue {
         .image__description_layer {
           opacity: 0;
           visibility: hidden;
+          top: 0;
+          height: auto;
+          align-items: flex-end;
         }
         &:hover .image__description_layer {
           visibility: visible;
@@ -189,8 +204,13 @@ export default class PhotoGallery extends Vue {
           transform: translateY(0);
         }
         .description__container {
+          display: block;
           font-size: 1.25rem;
           transform: translateY(1em);
+          padding: 4vmin;
+          h1 {
+            margin-bottom: 2vh;
+          }
         }
       }
     }
@@ -203,16 +223,19 @@ export default class PhotoGallery extends Vue {
           font-size: 0.875rem;
         }
       }
+      .grid {
+        grid-template-columns: repeat(auto-fill, minmax(18rem, 1fr));
+      }
     }
     @media (min-width: 1024px) {
       padding: $verticalPadding $horizontalPadding / 2;
       .grid {
-        grid-template-columns: repeat(3, minmax(12rem, 1fr));
+        grid-template-columns: repeat(auto-fill, minmax(24rem, 1fr));
       }
     }
     @media screen and (min-width: 1024px) and (min-height: 1023px) and (max-height: 1024px) and (orientation: landscape) {
       .grid {
-        grid-template-columns: repeat(2, minmax(12rem, 1fr));
+        grid-template-columns: repeat(2, minmax(14rem, 1fr));
       }
       .image__container .description__container {
         h1 {
@@ -225,7 +248,7 @@ export default class PhotoGallery extends Vue {
     }
     @media (min-width: 1024px) and (min-height: 1366px) {
       .grid {
-        grid-template-columns: repeat(2, minmax(12rem, 1fr));
+        grid-template-columns: repeat(2, minmax(14rem, 1fr));
       }
       .image__container .description__container {
         h1 {
@@ -239,7 +262,7 @@ export default class PhotoGallery extends Vue {
   }
   @media (min-width: 1024px) {
     &.col-3 .photoGallery__container .grid {
-      grid-template-columns: repeat(4, minmax(12rem, 1fr));
+      grid-template-columns: repeat(4, minmax(14rem, 1fr));
     }
   }
 }
